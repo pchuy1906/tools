95dabc1c3ad1abb6fe103afe5fb0214be9b565d0
pchuy1906 <pchuy1906@gmail.com>
2025-03-13 17:11:02 -0700

    convert POSCAR to dftb gen format

--- POSCAR_2_gen.py
+++ POSCAR_2_gen.py
@@ -1,75 +1,20 @@
-import os
+import sys
+module_path = "/g/g92/pham20/tools/others"
+sys.path.append(module_path)
+import utils
+
 import numpy as np
-from numpy import *
-import commands
 
-f_scale=1.0
 
-f  = open('POSCAR' ,"r")
-tmp = f.readline()
-tmp = f.readline()
+import argparse
+parser = argparse.ArgumentParser(description='making the larger system')
+# Arguments supported by the code.
+parser.add_argument("--file_POSCAR",           default="POSCAR", help=' POSCAR/CONTCAR')
 
-unitcell = np.zeros(shape=(3,3))
-tmp = f.readline()
-tmp = tmp.split()
-unitcell[0,:] = tmp
-tmp = f.readline()
-tmp = tmp.split()
-unitcell[1,:] = tmp
-tmp = f.readline()
-tmp = tmp.split()
-unitcell[2,:] = tmp
+args = parser.parse_args()
+file_POSCAR  = args.file_POSCAR
 
-print ("unitcell=",unitcell)
-unitcell = unitcell * f_scale
+natom, unitcell, atomList, xyz = utils.read_POSCAR(file_POSCAR)
 
-tmp = f.readline()
-atomNameList = tmp.split()
-print ("atomNameList=",atomNameList)
-asym_list = ' '.join(atomNameList)
-
-tmp = f.readline()
-atomNumList = tmp.split()
-atomNumList = map(int, atomNumList)
-print ("atomNumList=",atomNumList)
-
-natom = sum(atomNumList)
-print ("natom=", natom)
- 
-xyztype = f.readline().split()[0]
-print (xyztype)
-
-xyz = np.zeros(shape=(natom,3))
-for k in range(natom):
-    tmp = f.readline()
-    tmp = tmp.split()
-    xyz[k,:] =  tmp[:3]
-f.close()
-
-#print (xyz)
-if xyztype=="direct" or xyztype=="Direct":
-    print ("BBB")
-    Axyz = np.dot(xyz, unitcell)
-if xyztype=="Cartesian":
-    print ("AAA")
-    Axyz = xyz
-
-f2 = open('POSCAR2gen.gen', "w")
-f2.write("%-d %4s\n" %(natom, "S"))
-f2.write("%-s \n" %(asym_list))
-
-#atomNumList
-#atomNameList
-ixyz = 0
-for i in range(len(atomNameList)):
-    for j in range(atomNumList[i]):
-        f2.write("%-5d %5d %15.9f %15.9f %15.9f\n" %(ixyz+1, i+1, Axyz[ixyz,0], Axyz[ixyz,1], Axyz[ixyz,2] ))
-        ixyz += 1
-
-f2.write("%15.9f%15.9f%15.9f\n" % (0.0,0.0,0.0))
-for ixyz in range(3):
-    for icellxyz in unitcell[ixyz,:]:
-        f2.write("%15.9f" % icellxyz)
-    f2.write("\n")
-
-f2.close()
+fname = "POSCAR2gen.gen"
+utils.write_gen(fname, natom, unitcell, atomList, xyz)
