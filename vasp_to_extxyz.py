fdbd61608918ad22fba95e35ff3ad4b6496b9623
pchuy1906 <pchuy1906@gmail.com>
2025-04-01 16:31:44 -0700

    convert VASP OUTCAR to extxyz

--- vasp_to_extxyz.py
+++ vasp_to_extxyz.py
@@ -1,10 +1,17 @@
 from ase.io import read, write
+import numpy as np
 
 input_file = "OUTCAR"
 output_file = "output.extxyz"
 
 # Read all frames from the OUTCAR file
 atoms_list = read(input_file, index=":", format="vasp-out")
+
+#num_atoms = len(atoms_list[0])
+#q = np.zeros(num_atoms)
+q = np.loadtxt("qBader.dat")
+
+atoms_list[0].new_array('hirshfeld', q)
 
 # Write all frames to a single extxyz file
 write(output_file, atoms_list, format="extxyz")
